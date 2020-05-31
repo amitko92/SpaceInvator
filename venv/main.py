1d6c01d8ba42a91efeffc72cc01df7a687db4b35
Amit Koren <30484152+amitko92@users.noreply.github.com>
2020-05-31 17:47:59 +0300

    update

--- venv/main.py
+++ venv/main.py
@@ -6,9 +6,17 @@ pygame.init()
 # creating the screen.
 screen = pygame.display.set_mode((800,600))
 
+pygame.display.set_caption("Space Invader")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+olive_color = (128, 128, 0)
+
 # the game loop.
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # RGB - Red, Green, Blue
+        screen.fill(olive_color)
+        pygame.display.update()
